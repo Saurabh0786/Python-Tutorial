f1b485e3f4cc02b88dd1fdacdfd3e0af4041773b
Saurabh Shrivastav <sryder258@gmail.com>
2025-09-19 03:01:29 +0530

    Add files via upload

---
+++ Python_tutorial_1.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Basic assign value
+a=10
+print(a)
+
+
+# In[29]:
+
+
+# Hello World First Program
+print("Hello Python!")
+str="Python"
+print(f"Hello this is {str}")
+print(f"The addition of {a} and {b} is {a+b}")
+
+
+# In[11]:
+
+
+#Addition1 in python
+a=5+1
+print(a)
+
+
+# In[28]:
+
+
+#Addition2 in python
+a=12
+b=14
+c=a+b
+print(c)
+
+
+# In[22]:
+
+
+# Subtraction in python
+d=12
+e=8
+f=d-e
+print(f)
+
+
+# In[43]:
+
+
+# Datatype Check in Python
+int=5
+float=4.6
+str='Check'
+boolean='1'
+print(type(int))
+print(type(float))
+print(type(str))
+print(type(boolean))
+
+
+# In[46]:
+
+
+#tricky Question
+print(bool('1'))
+print(bool('0'))
+print(bool(''))
+
+
+# In[49]:
+
+
+print(bool('False'))  # True or False?
+print(bool(''))       # True or False?
+print(bool(0))        # True or False?
+print(bool([""]))       # True or False?
+
+
+# In[61]:
+
+
+# First letter Special Character is not allowed except underscore "_"
+@s="num"
+
+
+# In[57]:
+
+
+_city="Pune"
+print(_city)
+
+
+# In[63]:
+
+
+# First letter Integer is not allowed
+4day=30
+
+
+# In[64]:
+
+
+# Space is not allowed between word 
+roll num=5
+
+
+# In[1]:
+
+
+a=5.5
+print(type(a))
+b=str(a)
+print(type(b))
+
+
+# In[4]:
+
+
+a='20'
+print(type(a))
+b=int(a)
+print(type(b))
+
+
+# In[5]:
+
+
+#actually ye error isliye he string ki value me char he wo kbhi int me change ni hoga agr num hota to convert ho jata
+a='ab' 
+print(type(a))
+b=int(a)
+print(type(b))
+
+
+# In[8]:
+
+
+i=True
+print(type(i))
+j=int(i)
+print(type(j))
+
+
+# In[14]:
+
+
+#user input of addition,subtraction,multiplication
+i=float(input("Enter the First value"))
+j=float(input("Enter the Second value"))
+Addition=print("The Addition of first and second value is",i+j)
+Subtraction=print("The Subtraction of first and second value is",i-j)
+Multiplication=print("The Multiplication of first and second value is",i*j)
+Division=print("The Division of first and second value is",i/j)
+Modulo=print("The Modulo of first and second value is",i%j)
+Exponentiation=print("The Exponentiation of first and second value is",i**j)
+Quotient=print("The Quotient of first and second value is",i//j)
+
+
+# In[ ]:
+
+
+
+
